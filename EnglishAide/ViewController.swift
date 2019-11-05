d6d9fc1adb9f4901161a08895f905f46d9efe87a
guo yi <guoyi@guos-MacBook-Pro.local>
2019-11-05 21:36:22 +0800

    Initial commit

---
+++ EnglishAide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EnglishAide
+//
+//  Created by guo yi on 11/5/19.
+//  Copyright © 2019 guo yi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ EnglishAideTests/EnglishAideTests.swift
@@ -0,0 +1,34 @@
+//
+//  EnglishAideTests.swift
+//  EnglishAideTests
+//
+//  Created by guo yi on 11/5/19.
+//  Copyright © 2019 guo yi. All rights reserved.
+//
+
+import XCTest
+@testable import EnglishAide
+
+class EnglishAideTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
